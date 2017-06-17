ee122f4cea89de04c0488c8815afab63d3d1ae40
nb2017 <zerondrd@gmail.com>
2017-06-17 16:22:36 +0900

    add Comments

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
+//------------------------------------------------------------------------------------
+// Window onloadメソッド
+//------------------------------------------------------------------------------------
 window.onload = function() {
-  var size = 512
+  var size = 512;        //  画面サイズ
   var canvas = document.createElement('canvas');
-  canvas.width = size;
-  canvas.height = size;
+  canvas.width = size;        //  画面縦サイズ
+  canvas.height = size;        //  画面幅サイズ
   canvas.style.border = 'solid black 1px';
 
   document.body.appendChild(canvas);
 
+// MMDコントロールクラス生成
   var mmd = new MMD(canvas, canvas.width, canvas.height);
+// シェーダ初期化
   mmd.initShaders();
+// パラメータ初期化
   mmd.initParameters();
   mmd.registerKeyListener(document);
   mmd.registerMouseListener(document);
 
+// MMDモデル読み込み
   var miku = new MMD.Model('model', 'Miku_Hatsune_Ver2.pmd');
-  //var miku = new MMD.Model('Lat', 'Normal.pmd');
-  //var miku = new MMD.Model('mobko', 'mobko.pmd');
-  //var miku = new MMD.Model('yufu', 'yufu.pmd');
-  //var miku = new MMD.Model('defoko', 'defoko.pmd');
+  // 他のキャラでも良い？
+            //var miku = new MMD.Model('Lat', 'Normal.pmd');
+            //var miku = new MMD.Model('mobko', 'mobko.pmd');
+            //var miku = new MMD.Model('yufu', 'yufu.pmd');
+            //var miku = new MMD.Model('defoko', 'defoko.pmd');
   miku.load(function() {
     mmd.addModel(miku);
     mmd.initBuffers();
